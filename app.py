0e5d423aaf4b85514d0337a1f70b46e66ee653c4
bberg <benberg00@gmail.com>
2023-05-04 23:46:23 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,128 @@
+from flask import Flask, render_template, request, Response, stream_with_context
+from pprint import pprint as pp
+import openai
+import pinecone
+import time
+import random
+import pandas as pd
+import os
+
+print_all = True
+
+app = Flask(__name__)
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+pinecone_env = os.environ.get("PINECONE_ENV")
+
+df = pd.read_pickle('output.pkl')
+
+index_name = 'midrash'
+
+pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
+
+# check if index already exists (it shouldn't if this is first time)
+if index_name not in pinecone.list_indexes():
+    # if does not exist, create index
+    pinecone.create_index(
+        index_name,
+        dimension=1536,
+        metric='cosine',
+        metadata_config={'indexed': ['channel_id', 'published']}
+    )
+# connect to index
+pinecone_index = pinecone.Index(index_name)
+# view index stats
+print(pinecone_index.describe_index_stats())
+
+
+def get_embedding(text, model="text-embedding-ada-002"):
+   text = text.replace("\n", " ")
+   return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
+
+def search_function(query,texts):
+    context_plus_query = ''
+
+    for t in texts:
+        filename = '\nsource: '+str(t['filename'])
+        segment_number = '\t segment:'+str(t['segment_number'])
+        text = '\ntext:' + str(t['text']+'\n\n')
+        context_plus_query += filename
+        # context_plus_query += segment_number
+        context_plus_query += text
+        if print_all:
+            yield filename
+            yield segment_number
+            yield text
+    try:
+        context_plus_query += '\n\n --- \n\n + ' + query
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system",
+                 "content": "You are a Rabbi chatbot analyzing Midrash, analyze how each of the provided source texts specifically answers the question. Identify areas within or between the texts where different or conflicting advice is provided and explain what is meant by these contradictions. Cite the sources explicitly.  If your are unable to answer the question using the provided context, say 'I don't know'"},
+                {"role": "user", "content": context_plus_query}
+            ],
+            temperature=0.2,
+            stream=True
+        )
+        # create variables to collect the stream of chunks
+        collected_chunks = []
+        collected_messages = []
+        # iterate through the stream of events
+        for chunk in response:
+            pp(chunk)
+            collected_chunks.append(chunk)  # save the event response
+            try:
+                chunk_message = chunk['choices'][0]['delta']['content']  # extract the message
+            except KeyError:
+                chunk_message = ''
+
+            collected_messages.append(chunk_message)  # save the message
+            # print(chunk_message)
+            yield chunk_message
+    except Exception as e:
+        yield str(e)
+
+def search_function_debug(query):
+    start_time = time.time()
+    duration = 30  # Duration in seconds
+
+    while time.time() - start_time < duration:
+        random_number = random.randint(1, 100)  # Generate a random number between 1 and 100
+        yield f"data: {random_number}\n\n"  # Yield the random number as a Server-Sent Event
+        time.sleep(1)
+
+def get_relevant_sources(topic):
+    # retrieve from openai
+    xq = get_embedding(text=topic)
+
+    # get relevant contexts (including the questions)
+    res = pinecone_index.query(xq, top_k=3, include_metadata=True)
+
+    print(res)
+    # TODO then do a second pass within the top articles and identify the top 1 or two segments that pertain to the item requested
+
+    texts = []
+    for i in res['matches']:
+        # print(df.loc[int(i['id']),].to_dict())
+        texts.append(df.loc[int(i['id']),].to_dict())
+    return texts
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['search']
+    texts = get_relevant_sources(query)
+    pp(texts)
+    return Response(stream_with_context(search_function(query,texts)), content_type='text/event-stream')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
---
+++ generate-midrash-embeddings.py
@@ -0,0 +1,180 @@
+import openai
+import os
+import fnmatch
+import tiktoken
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.tokenize import sent_tokenize
+import time
+from pprint import pprint as pp
+import pinecone
+import pandas as pd
+
+
+nltk.download('punkt')
+
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+
+def find_text_files(directory):
+    text_files = []
+    for root, dirs, files in os.walk(directory):
+        if root.split('/')[-1] == 'English':
+            for file in files:
+                if fnmatch.fnmatch(file, '*.txt'):
+                    text_files.append(os.path.join(root, file))
+    return text_files
+
+def read_text_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    return content
+
+def create_dataframe(text_files,sentence_block_length):
+    data = []
+    for file in text_files:
+        content = read_text_file(file)
+        sentences = sent_tokenize(content)
+
+        # Split the text into segments of sentences
+        segments = [sentences[i:i + sentence_block_length] for i in range(0, len(sentences), sentence_block_length)]
+
+        for idx, segment in enumerate(segments):
+            segment_text = " ".join(segment)
+            segment_text = segment_text.replace('\n', ' ').replace('\r', ' ')
+            data.append([file, idx + 1, segment_text])
+
+    df = pd.DataFrame(data, columns=['filename', 'segment_number', 'text'])
+    return df
+
+# gets emeddings for the column 'text'
+def get_embedding(text, model="text-embedding-ada-002"):
+    text = text.replace("\n", " ")
+    result = openai.Embedding.create(input=[text], model=model)['data'][0]['embedding']
+    return result
+
+def text_stats(text_file_list):
+    total = ''
+    for t in text_files:
+        print(t)
+        total += read_text_file(t)
+
+    total_chars = len(total)
+    estimated_tokens = total_chars / 4.05
+    actual_tokens = num_tokens_from_string(total, "cl100k_base")
+    thousand_tokens = actual_tokens / 1000
+    estimated_cost_to_embed = 0.0004 * thousand_tokens
+
+    print('total files: ', len(text_files))
+    print("estimated_tokens", estimated_tokens)
+    print("actual_tokens", actual_tokens)
+    # print('total segments: ',thousand_tokens)
+    print("estimated_cost_to_embed: ", estimated_cost_to_embed)
+
+def initialize_pinecone(pinecone_api_key,pinecone_env,pinecone_index_name):
+    pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
+    # check if index already exists (it shouldn't if this is first time)
+    if pinecone_index_name not in pinecone.list_indexes():
+        # if does not exist, create index
+        pinecone.create_index(
+            pinecone_index_name,
+            dimension=1536,
+            metric='cosine',
+            metadata_config={'indexed': ['channel_id', 'published']}
+        )
+    # connect to index
+    index = pinecone.Index(pinecone_index_name)
+    # view index stats
+    print(index.describe_index_stats())
+    return index
+
+
+
+# CONFIG
+# todo move keys to a safe location
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+pinecone_env = os.environ.get("PINECONE_ENV")
+
+pinecone_index_name = 'midrash'
+
+# directory containing text files
+directory = 'Sefaria-Export/txt/Midrash'
+
+output_csv = 'output.csv'
+output_pkl = 'output.pkl'
+
+# how many sentences should each segment be? Total needs to be less than the token limit for ada
+sentence_block_length = 25
+# todo remove
+# 25 -> 51828
+# 50 -> 26070
+# 100 -> 13182
+# 1000 -> 1662
+
+
+if not os.path.exists(output_pkl):
+
+    # find the text files
+    text_files = find_text_files(directory)
+
+    # print some stats about the text files
+    text_stats(text_files)
+
+    # create a dataframe from the text files
+    df = create_dataframe(text_files,sentence_block_length)
+
+    print("numer of segments: ", df.shape[0])
+
+    # allows us to simply shorten the length of the dataframe for testing purposes
+    # df_test = df.head(10)
+    df_run = df
+    df_run['ada_embedding'] = np.empty((len(df_run), 0)).tolist()
+    df_run['uploaded_to_pinecone'] = False
+    df_run.to_csv(output_csv, index=False)
+    df_run.to_pickle(output_pkl)
+
+else:
+    print("LOADING FROM FILE: ",output_pkl)
+    df_run = pd.read_pickle(output_pkl)
+
+pinecone_index = initialize_pinecone(pinecone_api_key,pinecone_env,pinecone_index_name)
+
+
+batch_size = 100
+batch_count = 0
+for index, row in df_run.iterrows():
+    try:
+        if row['ada_embedding'] == []:
+            batch_count+=1
+            print(index)
+            try:
+                df_run.at[index,'ada_embedding'] = get_embedding(row['text'])
+            except Exception as e:
+                print(e)
+                print("retrying with first 6000 characters of text only")
+                df_run.at[index, 'ada_embedding'] = get_embedding(row['text'][:6000])
+        if batch_count > batch_size:
+            print('batch upload')
+            batch = df_run[index-batch_size:index]
+            # get tuples formatted for pinecone
+            batch_tuples = [(str(batch_index), batch_row['ada_embedding']) for batch_index, batch_row in batch.iterrows()]
+            # pp(batch_tuples)
+            # upload to pinecone
+            res = pinecone_index.upsert(vectors=batch_tuples)
+            # update the uploaded to pinecone column
+            df_run.loc[index - batch_size:index, 'uploaded_to_pinecone'] = True
+            # save the csv off so we don't lose progress
+            df_run.to_csv(output_csv, index=False)
+            df_run.to_pickle(output_pkl)
+            print('upload success')
+            batch_count = 0
+    except Exception as e:
+        print("ERROR")
+        print(e)
